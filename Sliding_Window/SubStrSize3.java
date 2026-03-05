4529c3371d36a08d1cc22da9f987667c84338aa6
unknown <devanshudeshmukh04@gmail.com>
2026-03-05 21:16:39 +0530

    44

---
+++ Sliding_Window/SubStrSize3.java
@@ -0,0 +1,18 @@
+package Sliding_Window;
+
+public class SubStrSize3 {
+    public static void main(String[] args) {
+        String s = "aababcabc";
+        int count = 0;
+        for (int i = 0; i < s.length() - 2; i++) {
+            char a = s.charAt(i);
+            char b = s.charAt(i + 1);
+            char c = s.charAt(i + 2);
+            if (a != b && b != c && a != c) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+}
