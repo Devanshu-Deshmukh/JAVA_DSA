38fec3b5707a9bc58ee2216e19d36c44134c2f91
unknown <devanshudeshmukh04@gmail.com>
2026-03-18 21:32:27 +0530

    61

---
+++ Prefix_Sum/Running_Sum.java
@@ -0,0 +1,14 @@
+package Prefix_Sum;
+
+import java.util.Arrays;
+
+public class Running_Sum {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4 };
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i - 1] + nums[i];
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+}
